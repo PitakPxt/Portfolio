1e735736884ea607b35ef12640f18b4ce4c0359a
Pitak Mennakret <pitakmennakret@MacBookProPitak.local>
2024-09-28 13:16:27 +0700

    add custom cursors

--- main.js
+++ main.js
@@ -82,3 +82,62 @@ closeBtn.addEventListener("click", closeOverlay);
     });
   });
 })();
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#d5d6d8",
+  "#96979e",
+  "#595a60",
+  "#54555b",
+  "#4f5055",
+  "#4a4b50",
+  "#45464b",
+  "#404146",
+  "#3c3c40",
+  "#37373a",
+  "#323235",
+  "#2d2d30",
+  "#2d2d30",
+  "#1e1e20",
+  "#19191b",
+  "#141415",
+  "#0f0f10",
+  "#0f0f10",
+  "#050506",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
